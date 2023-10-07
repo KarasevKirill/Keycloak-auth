bcf3690af5299e24c09f31a6bbd281b421ad6c09
Kirill Karasev <karasev.kirill.a@gmail.com>
2023-10-07 23:21:07 +1000

    Initial commit

---
+++ JWT/KeycloakJwtBearerHandler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+
+namespace KeycloakAuth.JWT
+{
+    public class KeycloakJwtBearerHandler : JwtBearerHandler
+    {
+        public KeycloakJwtBearerHandler(
+            IOptionsMonitor<JwtBearerOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock
+        ) : base(options, logger, encoder, clock) { }
+    }
+}
